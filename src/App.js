d4944e1cfdb94c607799916ca28c7f06ef54bfc1
Marek Mroz <marekmroz@wp.pl>
2019-04-10 13:15:26 +0200

    firebaseConf added

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
-import React from 'react';
+import React from 'react'
 
+import { database } from './firebaseConf'
+
+// const courseRef = database.ref('/JFDDL7')
+
+database.ref('/JFDDL7').once(
+  'value'
+)
+  .then(snapshot => console.log(snapshot.val()))
 
 const App = (props) => (
-  
-    <div>
-        App
+
+  <div>
+    App
     </div>
-  
+
 )
 
-
-export default App;
+export default App
--- src/firebaseConf.js
+++ src/firebaseConf.js
@@ -10,3 +10,5 @@ const config = {
   }
 
   firebase.initializeApp(config)
+
+  export const database = firebase.database()
